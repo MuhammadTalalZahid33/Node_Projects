226896e9e042b3d9a9a4d2a2cf77b029c22aec6b
Talal <talalzahid912@gmail.com>
2025-11-14 19:58:27 +0500

    used dotenv...

--- first-backend-proj/index.js
+++ first-backend-proj/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
+require('dotenv').config()
+
 const app = express()
-const port = 3000
+const port = 4000
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -14,6 +16,6 @@ app.get('/twitter', (req, res) => {
     res.send('Hello from twitter, now known as "x"');
 })
 
-app.listen(port, () => {
+app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`)
 })
