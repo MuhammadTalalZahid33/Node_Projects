fcc4925611f74f4bf41125b3063d596bb2e42033
Talal <talalzahid912@gmail.com>
2025-11-14 19:26:45 +0500

    started our first backend app...

--- first-backend-proj/index.js
+++ first-backend-proj/index.js
@@ -1,1 +1,19 @@
-console.log("hello world...")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/instagram', (req, res) => {
+    res.send('<h2>Instagram</h2> <br> You are now using instagram...')
+})
+
+app.get('/twitter', (req, res) => {
+    res.send('Hello from twitter, now known as "x"');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
