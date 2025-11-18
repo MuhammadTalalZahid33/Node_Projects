139b9bce19cccf4f9c532d35d3656ff114155bc6
Talal <talalzahid912@gmail.com>
2025-11-18 20:00:41 +0500

    connected db in the index file... using trycatch, async await...

---
+++ ffbackendProject/src/index.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+
+
+/*
+import express from "express";
+const app = express();
+; (async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${AtlasDB}`)
+        app.on("error", (error) => {
+            console.log("ERRR: ", error);
+            throw (error)
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port: ${process.env.PORT}`)
+        })
+
+    } catch (error) {
+        console.error("Error: ", error);
+        throw error
+    }
+})()
+*/
