270a9f28033035de9d79e13f75502e5c13f2d816
Talal <talalzahid912@gmail.com>
2025-11-19 17:02:13 +0500

    enhanced error handling in index.js for db connection and installed cors and cookie-parser and also the port is defined in the constants file...

--- ffbackendProject/src/constants.js
+++ ffbackendProject/src/constants.js
@@ -1,1 +1,2 @@
 export const DB_NAME = "AtlasDB"
+export const PORT = process.env.PORT
--- ffbackendProject/src/index.js
+++ ffbackendProject/src/index.js
@@ -2,10 +2,40 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
+import { PORT } from "./constants.js";
 
 dotenv.config({path: './.env'})
 
-connectDB();
+connectDB()
+.then(() => {
+    app.on("error" ,(error) => {
+        console.log("error listening event, express app unable to talk", error);
+        throw(error)
+    } )
+    app.listen(PORT || 3000, () => {
+        console.log(`Server is running at port ${PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("db connection error: ", error);
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// CONNECTING DB IN index.js 
 
 // import { DB_NAME  } from "./constants";
 // import express from "express";
