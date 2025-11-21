84c958f90992ad902668aaa416f17fb8e95b6e4a
Talal <talalzahid912@gmail.com>
2025-11-21 20:11:18 +0500

    asyncHandler implemented in utils...

---
+++ ffbackendProject/src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+
+// USING PROMISES
+const asyncHandler = (requestHandler) => (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next))
+    .catch((err) => next(err))
+}
+
+export {asyncHandler}
+
+// USING TRY-CATCH 
+// const asyncHandler = (fun) => async (req, res, next) => {
+//     try {
+//         await fun(req, res, next);
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false, 
+//             message: error.message,
+//         })       
+//     }
+// }
