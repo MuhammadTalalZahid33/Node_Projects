c3a196e53d50d4359ffa3a5a63c9dfb3feaa5ab1
Talal <talalzahid912@gmail.com>
2025-11-24 20:05:18 +0500

    built basic user and video schema, models using mongoose and installed mongoose-aggregate-paginate-v2 package

--- ffbackendProject/src/models/video.model.js
+++ ffbackendProject/src/models/video.model.js
@@ -1,1 +1,35 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import { User } from "./user.model";
+
+const videoSchema = mongoose.Schema({
+    videoFile: {
+        type: String, //Cloudinary link
+        required: true,
+    },
+    thumbnail:{
+        type: String, //Cloudinary link
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,
+        required: true,
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    isPublished:{
+        type: Boolean,
+        default: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    }
+}, {timestamps: true})
+
+export const Video = mongoose.model('Video', videoSchema);
